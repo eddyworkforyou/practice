7197b16713600192f1c3af2e45d6159cb595bf9c
eddyworkforyou <eddyworkforyou@gmail.com>
2017-03-04 13:19:56 +0800

    c

---
+++ exercise/webpack.config.js
@@ -0,0 +1,32 @@
+var wepack = require("webpack")
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: __dirname + "dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 3000
+	},
+	module: {
+	  	rules: [{
+		    test: /\.js$/,
+		    exclude: /node_modules/,
+		    loaders: [
+		      'babel-loader?' +
+		        'presets[]=es2016,' +
+		        'presets[]=stage-0,' +
+		        'presets[]=react' 
+		    ]
+	  	},
+	  	{
+	  		test: /\.json$/,
+	  		exclude: /(node_modules)/,
+	  		loader: "json-loader"
+	  	}] 
+	}
+}
