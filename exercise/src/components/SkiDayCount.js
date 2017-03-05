38b17dcd9c870d0832ab8fabc4f63f60c4e7fcec
eddyworkforyou <eddyworkforyou@gmail.com>
2017-03-05 12:23:28 +0800

    c

--- exercise/src/components/SkiDayCount.js
+++ exercise/src/components/SkiDayCount.js
@@ -1,32 +1,36 @@
-import React from 'react'
+import { Component } from 'react'
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
-export const SkiDayCount = React.createClass({
-	percentToDecimal(decimal) {
-		return ((decimal * 100) + '%')
-	},
-	calcGoalProgress(total, goal) {
-		return this.percentToDecimal(total/goal)
-	},
-	render() {
-		return (
-			<div className="ski-day-count">
-				<div className="total-days">
-					<span>{this.props.total}</span>
-					<span>days</span>
-				</div>
-				<div className="powder-days">
-					<span>{this.props.powder}</span>
-					<span>days</span>
-				</div>
-				<div className="backcountry-days">
-					<span>{this.props.backcountry}</span>
-					<span>days</span>
-				</div>
-				<div>
-					<span>{this.calcGoalProgress(this.props.total, this.props.goal)}</span>
-				</div>
-			</div>
-		)
-	}
-})
+const percentToDecimal = (decimal) => {
+	return ((decimal * 100) + '%')
+}
+
+const calcGoalProgress = (total, goal) => {
+	return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+	<div className="ski-day-count">
+		<div className="total-days">
+			<span>{total}</span>
+				<Calendar />
+			<span>days</span>
+		</div>
+		<div className="powder-days">
+			<span>{powder}</span>
+				<SnowFlake />
+			<span>days</span>
+		</div>
+		<div className="backcountry-days">
+			<span>{backcountry}</span>
+				<Terrain />
+			<span>days</span>
+		</div>
+		<div>
+			<span>{calcGoalProgress(total, goal)}</span>
+		</div>
+	</div>
+)
